323ba2c177394aa4f7ef264024f86d601d11a940
Guillermo <g.gonzalez.caparros@gmail.com>
2025-03-16 21:18:20 +0100

    feat: tests for FlightServiceImpl

--- src/main/java/com/ryanair/task2/dto/output/InterconnectionDTO.java
+++ src/main/java/com/ryanair/task2/dto/output/InterconnectionDTO.java
@@ -1,6 +1,9 @@
 package com.ryanair.task2.dto.output;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Objects;
+
 
 public record InterconnectionDTO(
         int stops,
@@ -10,5 +13,40 @@ public record InterconnectionDTO(
             String arrivalAirport,
             LocalDateTime departureDateTime,
             LocalDateTime arrivalDateTime) {
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            LegDTO legDTO = (LegDTO) o;
+
+            return departureAirport.equals(legDTO.departureAirport) &&
+                    arrivalAirport.equals(legDTO.arrivalAirport) &&
+                    departureDateTime.equals(legDTO.departureDateTime) &&
+                    arrivalDateTime.equals(legDTO.arrivalDateTime);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(departureAirport, arrivalAirport, departureDateTime, arrivalDateTime);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        InterconnectionDTO that = (InterconnectionDTO) o;
+
+        if (stops != that.stops) return false;
+
+        return Arrays.equals(legs, that.legs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stops) + Arrays.hashCode(legs);
     }
 }
---
+++ src/test/java/com/ryanair/task2/domain/services/impl/FlightServiceImplTest.java
@@ -0,0 +1,77 @@
+package com.ryanair.task2.domain.services.impl;
+
+import com.ryanair.task2.domain.model.Schedule;
+import com.ryanair.task2.domain.services.RouteService;
+import com.ryanair.task2.domain.services.ScheduleService;
+import com.ryanair.task2.dto.output.InterconnectionDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class FlightServiceImplTest {
+    static final LocalDateTime UTIL_DATE_TIME = LocalDateTime.of(2021, 1, 1, 0, 0);
+
+    @Mock
+    private RouteService routeService;
+
+    @Mock
+    private ScheduleService scheduleService;
+
+    private FlightServiceImpl flightServiceImpl;
+
+    @BeforeEach
+    void setUp() {
+        flightServiceImpl = new FlightServiceImpl(routeService, scheduleService);
+    }
+
+    @Test
+    void testGetInterconnections() {
+        when(routeService.getItineraries(anyString(), anyString(), anyInt(), anyString()))
+                .thenReturn(Flux.fromIterable(createItinerary()));
+
+        when(scheduleService.getSchedulesFromItinerary(anyList(), any(LocalDateTime.class), any(LocalDateTime.class), anyInt()))
+                .thenReturn(Flux.fromIterable(createSchedules()));
+
+        List<InterconnectionDTO> expectedResult = List.of(
+                new InterconnectionDTO(2, new InterconnectionDTO.LegDTO[]{
+                        new InterconnectionDTO.LegDTO("A", "B", UTIL_DATE_TIME, UTIL_DATE_TIME),
+                        new InterconnectionDTO.LegDTO("B", "C", UTIL_DATE_TIME, UTIL_DATE_TIME)
+                }),
+                new InterconnectionDTO(2, new InterconnectionDTO.LegDTO[]{
+                        new InterconnectionDTO.LegDTO("A", "B", UTIL_DATE_TIME, UTIL_DATE_TIME),
+                        new InterconnectionDTO.LegDTO("B", "C", UTIL_DATE_TIME, UTIL_DATE_TIME)
+                })
+        );
+
+        List<InterconnectionDTO> result = flightServiceImpl.getInterconnections("DUB", "STN", UTIL_DATE_TIME, UTIL_DATE_TIME).collectList().block();
+
+        assertEquals(expectedResult, result);
+    }
+
+    private static List<List<String>> createItinerary() {
+        return List.of(List.of("A", "B", "C"));
+    }
+
+    private static List<List<Schedule>> createSchedules() {
+        return List.of(
+                List.of(
+                        new Schedule(UTIL_DATE_TIME, UTIL_DATE_TIME),
+                        new Schedule(UTIL_DATE_TIME, UTIL_DATE_TIME)),
+                List.of(
+                        new Schedule(UTIL_DATE_TIME, UTIL_DATE_TIME),
+                        new Schedule(UTIL_DATE_TIME, UTIL_DATE_TIME))
+        );
+    }
+}
